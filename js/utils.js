b7b9a0647301dcef7ad1ca40d21a1058520ce6f2
Ghabryel Henrique <ghabryelcode@gmail.com>
2022-08-10 22:14:54 -0300

    first commit

---
+++ js/utils.js
@@ -0,0 +1,40 @@
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
+}
+
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId);
+  document.querySelector("#displayText").style.display = "flex";
+  if (player.health === enemy.health) {
+    document.querySelector("#displayText").innerHTML =
+      "Tie<br><br> Press F5 to start again";
+  } else if (player.health > enemy.health) {
+    document.querySelector("#displayText").innerHTML =
+      "Player 1 Wins<br><br> Press F5 to start again";
+  } else if (player.health < enemy.health) {
+    document.querySelector("#displayText").innerHTML =
+      "Player 2 Wins<br><br> Press F5 to start again";
+  }
+}
+
+let timer = 60;
+let timerId;
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+
+  if (timer === 0) {
+    determineWinner({ player, enemy, timerId });
+  }
+}
